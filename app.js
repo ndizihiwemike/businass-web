9910d33b3fb7f464dfb93ae9875e92f0e0a04dd1
ndizihiwemike <142516647+ndizihiwemike@users.noreply.github.com>
2024-06-25 15:00:40 +0300

    Update app.js

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ app.post("/submit-form", async (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
